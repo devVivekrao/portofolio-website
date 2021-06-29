a4f4010fbc38d3da03844d14283af062a6253f2e
vivek rao <dev.vivekrao.com>
2021-06-29 19:26:54 +0530

    adding hover effet

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-
+import Github from "../assets/icons/github2.svg";
+import Eye from "../assets/icons/eye.svg";
 function Project({ project }) {
   return (
     <div className="project">
+      <div className="hover">
+        <a href="http://netlify.com" target="_blank" rel="noopener noreferrer">
+          <img src={Eye} alt="eye" />
+          <br />
+          view live
+        </a>
+        <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+          <img src={Github} alt="github" className="github" /> <br />
+          view code
+        </a>
+      </div>
       <img src={project.image} alt="img" />
       <h4>{project.name}</h4>
       <p>{project.description}</p>
